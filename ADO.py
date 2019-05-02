42fc74283f45ce62b4fde44f1579687708f7de3d
Hod Alpert <hod.alpert@stratoscalse.com>
2019-05-02 08:20:24 +0300

    preprocessing- generating Ai's

--- ADO.py
+++ ADO.py
@@ -1,7 +1,12 @@
+import numpy as np
+
+
 class ADO(object):
     """
 
     """
+    k = 0
+
     def compute_distance(self, u, v):
         """
         computes the distances according to the algorithm
@@ -20,7 +25,23 @@ class ADO(object):
         :param G: A graph represented as networkx graph
         :returns: a tuple (Lambda, P, B):
         """
-        pass
+        A = self.generate_A(G)
+        while not A[self.k - 1]:  # repeat until A[k-1] is not an empty set
+            A = self.generate_A(G)
+        # generating a list the size of (k * n) which will contain lambda(A[i], v) for any v in V initialized as -1
+        # symbolising infinity
+        distance_between_ais_and_nodes = [[-1 for i in range(len(G.nodes))] for i in range(self.k + 1)]
+
+    def generate_A(self, G):
+        A = [set() for i in range(self.k + 1)]  # initializing with k + 1 empty sets
+        A[0] = set(G.nodes.keys())  # A[0] = V
+        n = len(G.nodes)  # assigning n with # nodes
+        p = 1 / (n ** (1 / float(self.k)))  # assigning p with n^(-1/k)
+        for i in xrange(1, self.k):
+            for node in A[i - 1]:
+                if np.random.binomial(1, p, 1)[0]:
+                    A[i].add(node)
+        return A
 
     def parse_graph(self, *argv, **kwargs):
         """
@@ -34,4 +55,3 @@ class ADO(object):
         TODO
         """
         pass
-
