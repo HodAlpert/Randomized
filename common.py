89016b7d3dbef4800b3cc2df05b244d6e3babe7d
Hod Alpert <hod.alpert@stratoscalse.com>
2019-06-03 13:26:17 +0300

    finished implementing the algorithm (one unanswered question remains though)

---
+++ common.py
@@ -0,0 +1,58 @@
+import time
+
+
+def modified_dijkstra(G, source, i, Lambda):
+    assert source in G.nodes, 'Such source node doesn\'t exist'
+
+    # 1. Mark all nodes unvisited and store them.
+    # 2. Set the distance to zero for our initial node
+    # and to infinity for other nodes.
+    distances = {vertex: float('inf') for vertex in G.nodes}
+    previous_vertices = {
+        vertex: None for vertex in G.nodes
+    }
+    distances[source] = 0
+    vertices = dict(G.nodes).copy()
+
+    while vertices:
+        # 3. Select the unvisited node with the smallest distance,
+        # it's current node now.
+        current_vertex = min(
+            vertices, key=lambda vertex: distances[vertex])
+
+        # 6. Stop, if the smallest distance
+        # among the unvisited nodes is infinity.
+        if distances[current_vertex] == float('inf'):
+            break
+
+        # 4. Find unvisited neighbors for the current node
+        # and calculate their distances through the current node.
+        for neighbour in G[current_vertex]:
+            weight = G[current_vertex][neighbour]['weight']
+            alternative_route = distances[current_vertex] + weight
+
+            # Compare the newly calculated distance to the assigned
+            # and save the smaller one.
+            if alternative_route < Lambda[i + 1][current_vertex]:  # modification of dijkstra
+                distances[neighbour] = min(alternative_route, distances[neighbour])
+                previous_vertices[neighbour] = current_vertex
+
+        # 5. Mark the current node as visited
+        # and remove it from the unvisited set.
+        vertices.pop(current_vertex)
+    return distances
+
+
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((te - ts) * 1000)
+        else:
+            print '%r  %2.2f ms' % \
+                  (method.__name__, (te - ts) * 1000)
+        return result
+    return timed
--- dist.py
+++ dist.py
@@ -1,15 +1,14 @@
 import random
-
-import networkx as nx
-
-from ADO import ADO
 import draw_graph
+import networkx as nx
+from ADO import ADO
+
+
 
 P = [[]]
 Lambda = [[]]
 B = []
 k = 3
-G = nx.complete_graph(5, nx.Graph())
 
 
 def compute_distance(u, v):
@@ -25,11 +24,24 @@ def compute_distance(u, v):
 
 
 if __name__ == '__main__':
-    G = nx.complete_graph(6, nx.Graph())
+    number_of_nodes = 200
+    k = 4
+    G = nx.complete_graph(number_of_nodes, nx.Graph())
     for v in G.nodes:
         for u in G.nodes:
             if (u, v) in G.edges:
                 G[u][v]['weight'] = 0 if u == v else int(random.random() * 40)
 
-    draw_graph.draw(G)
-    ADO().pre_processing(G)
+    # draw_graph.draw(G)  # how to draw the graph with it's weights
+    t = ADO(G, k)
+    t.pre_processing()
+    path_lengths, paths = nx.single_source_dijkstra(G, 1)
+    path_lengths.pop(1)
+    algorithm_distances = {v: t.compute_distance(1, v) for v in set(G) - {1}}
+    print "dijkstra length: ", path_lengths
+    print "algorithm length: ", algorithm_distances
+    average = 0.0
+    for i in set(G) - {1}:
+        average += float(abs(path_lengths[i] - algorithm_distances[i]))
+    average /= len(G) - 1
+    print "average stretch is ", average
